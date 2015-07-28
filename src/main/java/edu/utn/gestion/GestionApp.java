f1a3ed8c628d2842eb19ffcdbad89b2aad4b481a
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2015-07-28 20:13:07 -0300

    First commit.

---
+++ src/main/java/edu/utn/gestion/GestionApp.java
@@ -0,0 +1,11 @@
+package edu.utn.gestion;
+
+public class GestionApp {
+    
+    /**
+        * @param args the command line arguments
+        */
+    public static void main(String[] args) {
+        
+    }
+}
