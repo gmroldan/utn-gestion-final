4274269984d5046c7ad67187023f9509ab5b02c3
Gerardo Martín Roldán <gmroldanutn@gmail.com>
2016-09-26 01:30:42 -0300

    Refactor - Añadir mensajes de log en el mètodo BookService#decreaseStock.

--- src/main/java/edu/utn/gestion/service/BookService.java
+++ src/main/java/edu/utn/gestion/service/BookService.java
@@ -8,7 +8,9 @@ import edu.utn.gestion.exception.GestionAppException;
 import edu.utn.gestion.model.Book;
 import edu.utn.gestion.model.Category;
 import edu.utn.gestion.service.generic.GenericService;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.Validate;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
@@ -18,6 +20,8 @@ import java.util.List;
  */
 public class BookService extends GenericService<Book, String> {
     private static final BookService INSTANCE = new BookService();
+    private  static final Logger LOGGER = Logger.getLogger(BookService.class);
+
     private final BookDAO bookDAO = BookDAO.getInstance();
 
     /**
@@ -67,11 +71,21 @@ public class BookService extends GenericService<Book, String> {
     /**
      * Decreases the stock of a single book and then updates it into the DB.
      *
-     * @param bookId
+     * @param bookId Cannot be null or empty.
      * @param quantity
      * @throws GestionAppException If can't find the book. Or if the new stock is less than 0. Or if there is an issue trying to update the book.
      */
-    public void decreaseStock(String bookId, int quantity) throws GestionAppException {
+    public void decreaseStock(final String bookId, final int quantity)
+            throws GestionAppException {
+
+        if (StringUtils.isEmpty(bookId)) {
+            String errorMessage = "Cannot decrease the stock for a book without the bookId.";
+            LOGGER.error(errorMessage);
+            throw new GestionAppException(errorMessage);
+        }
+
+        LOGGER.info("Updating stock for the book with id = " + bookId);
+
         Book book = null;
 
         try {
